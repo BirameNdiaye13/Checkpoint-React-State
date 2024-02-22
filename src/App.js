7e67dba59b74c057a2b53cc9c6a07f7c746f397a
MacBook Retina <macbookretina@192.168.1.5>
2024-02-22 01:53:47 +0000

    React-State

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        fullName: "Birame NDIAYE",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        imgSrc: "../birame.png",
+        profession: "Développeur Web"
+      },
+      show: false,
+      elapsedTime: 0
+    };
+  }
+
+  toggleShow = () => {
+    this.setState(prevState => ({
+      show: !prevState.show
+    }));
+  }
+
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.setState(prevState => ({
+        elapsedTime: prevState.elapsedTime + 1
+      }));
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.toggleShow}>Toggle Show</button>
+        {this.state.show && (
+          <div>
+            <h1>{this.state.person.fullName}</h1>
+            <p>{this.state.person.bio}</p>
+            <img src={this.state.person.imgSrc} alt="Person" />
+            <p>{this.state.person.profession}</p>
+          </div>
+        )}
+        <p>Elapsed Time: {this.state.elapsedTime} seconds</p>
+      </div>
+    );
+  }
 }
 
 export default App;
